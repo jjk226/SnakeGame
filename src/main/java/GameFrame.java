ba87e7d207d041ed999ea1859edbea121144f6b2
James Kim <jkim670@gmail.com>
2021-03-14 00:13:52 -0500

    added game restart functionality

--- src/main/java/GameFrame.java
+++ src/main/java/GameFrame.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 
-public class GameFrame extends JFrame implements ActionListener {
+public class GameFrame extends JFrame {
 
     private static GamePanel panel;
     private JButton button;
@@ -13,37 +13,13 @@ public class GameFrame extends JFrame implements ActionListener {
         this.setSize(1000, 1000);
         this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        button = new JButton();
-        button.setText("Reset");
-        button.setSize(50, 25);
-        button.setLocation(0, 50);
-        button.addActionListener(this);
-        button.setVisible(true);
-
         this.panel = new GamePanel();
-
-        this.add(button);
         this.add(panel);
-
         this.setVisible(true);
         this.setResizable(false);
         this.pack();
         this.setLocationRelativeTo(null);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == (this.button)) {
-            this.remove(panel);
-            this.panel = new GamePanel();
-            this.add(panel);
-            SwingUtilities.updateComponentTreeUI(this);
-        }
-    }
-
-
-    //need to add restart functionanlity here at the Frame level??
 
 }
